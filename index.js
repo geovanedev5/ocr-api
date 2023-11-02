6568aa231b4b14e477f0f914112d30a20c79c7fe
geovanedev5 <148703357+geovanedev5@users.noreply.github.com>
2023-11-02 12:29:11 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const Tesseract = require('tesseract.js');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/ocr', (req, res) => {
+  const imageUrl = req.query.image;
+
+  if (!imageUrl) {
+    return res.status(400).json({ error: 'Parâmetro "image" ausente na URL' });
+  }
+
+  Tesseract.recognize(
+    imageUrl,
+    'eng',
+    { logger: (info) => console.log(info) }
+  )
+    .then(({ data: { text } }) => {
+      res.json({ text });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ error: 'Erro durante o OCR' });
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor está ouvindo na porta ${port}`);
+});
